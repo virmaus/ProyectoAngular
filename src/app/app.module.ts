92662bcc938dbb64092c8378dec35e1471c7f1e9
Your Name <you@example.com>
2022-09-09 21:35:04 -0400

    creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
-import { ContactoComponent } from 'src/components/contacto/contacto.component';
-import { AboutComponent } from 'src/components/about/about.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactoComponent,
-    AboutComponent
+    AboutComponent,
+    ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule} from "@angular/router";
+
+import { AppComponent } from './app.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
+const appRoutes: Routes = [
+    {path: '', component: AboutComponent},
+    {path: 'sobre-mi', component: AboutComponent},
+    {path: 'proyectos', component: ProjectsComponent},
+    {path: 'contacto', component: ContactoComponent},
+    {path: '**', component: AboutComponent}
+
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
