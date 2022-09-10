aa862a12cf2c5c357ef2211b8c34a66e02ce32c0
Your Name <you@example.com>
2022-09-09 20:34:40 -0400

    probando cosas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactoComponent } from 'src/components/contacto/contacto.component';
+import { AboutComponent } from 'src/components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
