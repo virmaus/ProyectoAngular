07a0ff95be0a53538bf116563caba4aba48cf971
Your Name <you@example.com>
2022-09-23 15:38:14 -0300

    actualizar componentes

--- src/app/components/cursos/cursos.component.ts
+++ src/app/components/cursos/cursos.component.ts
@@ -12,11 +12,12 @@ export class CursosComponent implements OnInit {
   public cursos: Array<Curso> = [];
   constructor() {
     this.cursos = [
-      new Curso('Master en Javascript: Aprender JS,JQuery,Angular,NodeJS','07-09-2022','Victor Robles'),
+      new Curso('Master en Javascript: Aprender JS, JQuery, Angular, NodeJS','07-09-2022','Victor Robles'),
       new Curso('PostgreSQL. Curso completo de PostgreSQL','En curso','Redait Media'),
       new Curso('Spring Framework 5 & Spring Boot 2 desde 0 a experto 2022','En curso','Andrés José Guzmán'),
       new Curso('Angular Framework Desde Cero','En curso','Yirsis Serrano')
       ];
+      
     }
 
   ngOnInit(): void {
